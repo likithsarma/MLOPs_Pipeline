ffc7f140e80f91d78f33ca427773ca9e77d1f7a8
Likith <likithjandhyala@gmail.com>
2026-01-28 14:01:12 +0530

    experiment tracking using dvclive done

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -5,6 +5,8 @@ import pickle
 import json
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import logging
+from dvclive import Live
+import yaml
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
@@ -22,6 +24,23 @@ console_handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 def load_model(model_path: str):
     """Load a trained model from a file."""
@@ -89,6 +108,8 @@ def save_metrics(metrics: dict, file_path: str) -> None:
 
 def main():
     try:
+        params = load_params(params_path='params.yaml')
+        # model_path = params['model_training']['model_path']
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
 
@@ -97,6 +118,12 @@ def main():
 
         metrics = evaluate_model(clf,X_test,y_test)
 
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test))
+            live.log_metric('recall', recall_score(y_test, y_test))
+
+            live.log_params(params)
         save_metrics(metrics, 'reports/metrics.json')
 
     except Exception as e:
